d0c602d4460a67a1472e1101ae5068ce58fa7a04
Julia Apreleva <12630m@mail.ru>
2020-11-26 19:24:25 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   var link = document.querySelector('.page-header__button');
   var modal = document.querySelector('.modal');
   var close = modal.querySelector('.modal__close');
-  var form = document.querySelectorAll('.form');
+  var form = document.querySelector('.form');
   var modalForm = document.querySelector('.modal__form');
   var userName = form.querySelector('[name=username]');
   var phone = form.querySelector('[name=phone]');
